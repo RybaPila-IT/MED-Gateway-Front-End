40ab4171ea881818b4674504f83119a9dc948704
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-03-21 20:28:46 +0100

    Add basic routing and NotFound page
    Implement basic routing functionality
    for public pages.

--- src/routes/public/Nav.js
+++ src/routes/public/Nav.js
@@ -1,8 +1,24 @@
+import {Link, Outlet} from "react-router-dom";
+import {AbsolutePaths} from "../Paths";
+
 function Nav() {
     return (
-        <nav>
-            This is my nav-bar...
-        </nav>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={AbsolutePaths.home}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={AbsolutePaths.login}>Login</Link>
+                    </li>
+                    <li>
+                        <Link to={AbsolutePaths.register}>Register</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
     )
 }
 
