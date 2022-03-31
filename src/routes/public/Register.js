f61bfb0dcd54e820a49d6119c57ebdf417c5ce2a
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-03-31 12:34:28 +0200

    Add basic css styling to center form

--- src/routes/public/Register.js
+++ src/routes/public/Register.js
@@ -1,3 +1,4 @@
+import './Register.css'
 import {FaUser} from "react-icons/fa";
 
 function Register() {
@@ -11,92 +12,97 @@ function Register() {
                     Please, fill below form in order to register
                 </p>
             </section>
-            <form>
-                <div className="mb-3">
-                    <label
-                        htmlFor="nameFormInput"
-                        className="form-label">
-                        Your name
-                    </label>
-                    <input
-                        id="nameFormInput"
-                        type="text"
-                        className="form-control"
-                        placeholder="Please, enter your name"/>
-                </div>
-                <div className="mb-3">
-                    <label
-                        htmlFor="surnameFormInput"
-                        className="form-label">
-                        Your surname
-                    </label>
-                    <input
-                        id="surnameFormInput"
-                        type="text"
-                        className="form-control"
-                        placeholder="Please, enter your surname"/>
-                </div>
-                <div className="mb-3">
-                    <label
-                        htmlFor="mailFormInput"
-                        className="form-label">
-                        Email address
-                    </label>
-                    <input
-                        id="mailFormInput"
-                        type="email"
-                        className="form-control"
-                        aria-describedby="emailInputDescription"
-                        placeholder="Please, enter your email"/>
-                    <div
-                        id="emailInputDescription"
-                        className="form-text">
-                        We'll never share your email with anyone else.
+            <form className="form">
+                <div className="form-group">
+                    <div className="mb-3">
+                        <label
+                            htmlFor="nameFormInput"
+                            className="form-label">
+                            Your name
+                        </label>
+                        <input
+                            id="nameFormInput"
+                            type="text"
+                            className="form-control"
+                            placeholder="Please, enter your name"/>
+                    </div>
+                    <div className="mb-3">
+                        <label
+                            htmlFor="surnameFormInput"
+                            className="form-label">
+                            Your surname
+                        </label>
+                        <input
+                            id="surnameFormInput"
+                            type="text"
+                            className="form-control"
+                            placeholder="Please, enter your surname"/>
+                    </div>
+                    <div className="mb-3">
+                        <label
+                            htmlFor="mailFormInput"
+                            className="form-label">
+                            Email address
+                        </label>
+                        <input
+                            id="mailFormInput"
+                            type="email"
+                            className="form-control"
+                            aria-describedby="emailInputDescription"
+                            placeholder="Please, enter your email"/>
+                        <div
+                            id="emailInputDescription"
+                            className="form-text">
+                            We'll never share your email with anyone else.
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <label
+                            htmlFor="organizationFormInput"
+                            className="form-label">
+                            Organization
+                        </label>
+                        <input
+                            id="organizationFormInput"
+                            type="text"
+                            className="form-control"
+                            aria-describedby="organizationInputDescription"
+                            placeholder="Please, enter your organization"/>
+                        <div
+                            id="organizationInputDescription"
+                            className="form-text">
+                            We would like to know which organization you represent.
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <label
+                            htmlFor="passwordInput"
+                            className="form-label">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="passwordInput"
+                            placeholder="Please, enter your password"/>
+                    </div>
+                    <div className="mb-3">
+                        <label
+                            htmlFor="passwordRepeatInput"
+                            className="form-label">
+                            Confirm your password
+                        </label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="passwordRepeatInput"
+                            placeholder="Confirm password"/>
+                    </div>
+                    <div className="btn-group" >
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-secondary">Clear</button>
                     </div>
                 </div>
-                <div className="mb-3">
-                    <label
-                        htmlFor="organizationFormInput"
-                        className="form-label">
-                        Organization
-                    </label>
-                    <input
-                        id="organizationFormInput"
-                        type="text"
-                        className="form-control"
-                        aria-describedby="organizationInputDescription"
-                        placeholder="Please, enter your organization"/>
-                    <div
-                        id="organizationInputDescription"
-                        className="form-text">
-                        We would like to know which organization you represent.
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <label
-                        htmlFor="passwordInput"
-                        className="form-label">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="passwordInput"
-                        placeholder="Please, enter your password"/>
-                </div>
-                <div className="mb-3">
-                    <label
-                        htmlFor="passwordRepeatInput"
-                        className="form-label">
-                        Repeat your password
-                    </label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="passwordRepeatInput"
-                        placeholder="Repeat password"/>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
     )
