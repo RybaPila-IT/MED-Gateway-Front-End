baaef2c0c50825823e633b3666ff9fb4555fdb51
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-04-11 14:33:23 +0200

    Make automatic login redirections
    What? Login page redirects users to
    protected Home Page if they are already
    authenticated.

--- src/routes/public/Login.js
+++ src/routes/public/Login.js
@@ -1,12 +1,14 @@
 import {useEffect, useState} from 'react';
+import {Navigate, useNavigate} from "react-router-dom";
 import {addHours} from "date-fns";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {login, userDataSessionKey} from '../../state/features/authorization'
 import {FaSignInAlt} from "react-icons/fa";
 import {useLoginUserMutation} from "../../api/backend";
 
 function Login() {
 
+    const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const [makeLoginRequest, {
@@ -94,81 +96,87 @@ function Login() {
         const dispatchLoginAction = () => {
             dispatch(login(data));
         }
+        const navigateToProtectedHomePage = () => {
+            navigate('/auth/')
+        }
 
         if (!data || !isSuccess) {
             return
         }
         storeUserData();
         dispatchLoginAction();
+        navigateToProtectedHomePage();
         // Not providing dispatch as the dependency.
     }, [isSuccess, data]) // eslint-disable-line react-hooks/exhaustive-deps
 
-    return (
-        <div>
-            <section>
-                <h1>
-                    <FaSignInAlt/>Login
-                </h1>
-                <p>
-                    Provide your credentials in order to login.
-                </p>
-            </section>
-            <form className="form centered-container" onSubmit={submitLoginRequest} onReset={resetForm}>
-                <div className="form-group">
-                    <div className="mb-3">
-                        <input
-                            id="mailFormInput"
-                            type="email"
-                            className="form-control"
-                            onChange={onChange}
-                            required={true}
-                            name="email"
-                            value={email}
-                            placeholder="Please, enter your email"/>
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            id="passwordInput"
-                            type="password"
-                            className="form-control"
-                            onChange={onChange}
-                            required={true}
-                            minLength="5"
-                            name="password"
-                            value={password}
-                            placeholder="Please, enter your password"/>
-                    </div>
-                    <div className="btn-group">
-                        <button type="submit" className="btn btn-primary" disabled={isLoading}>Login</button>
-                        <button type="reset" className="btn btn-secondary" disabled={isLoading}>Clear</button>
-                    </div>
-                </div>
-            </form>
-            <section>
-                <div className="centered-container">
-                    {
-                        isLoginError &&
-                        <div className="alert alert-danger" role="alert">
-                            {loginErrorMessage}
+    const {_id, state, token} = useSelector(state => state.authentication);
+    const authenticated = function () {
+        return _id !== null &&
+            state !== null &&
+            token !== null;
+    }();
+
+    return authenticated ?
+        <Navigate to={'/auth'}/> :
+        (<div>
+                <section>
+                    <h1>
+                        <FaSignInAlt/>Login
+                    </h1>
+                    <p>
+                        Provide your credentials in order to login.
+                    </p>
+                </section>
+                <form className="form centered-container" onSubmit={submitLoginRequest} onReset={resetForm}>
+                    <div className="form-group">
+                        <div className="mb-3">
+                            <input
+                                id="mailFormInput"
+                                type="email"
+                                className="form-control"
+                                onChange={onChange}
+                                required={true}
+                                name="email"
+                                value={email}
+                                placeholder="Please, enter your email"/>
                         </div>
-                    }
-                    {
-                        isLoading &&
-                        <div>
-                            <div className="spinner-border text-primary" role="status" />
-                            <span className="text-primary">Loading...</span>
+                        <div className="mb-3">
+                            <input
+                                id="passwordInput"
+                                type="password"
+                                className="form-control"
+                                onChange={onChange}
+                                required={true}
+                                minLength="5"
+                                name="password"
+                                value={password}
+                                placeholder="Please, enter your password"/>
                         </div>
-                    }
-                    {
-                        isSuccess &&
-                        <div className="alert alert-success" role="alert">
-                            {`Successfully logged in.`}
+                        <div className="btn-group">
+                            <button type="submit" className="btn btn-primary" disabled={isLoading}>Login</button>
+                            <button type="reset" className="btn btn-secondary" disabled={isLoading}>Clear</button>
                         </div>
-                    }
-                </div>
-            </section>
-        </div>
-    )
+                    </div>
+                </form>
+                <section>
+                    <div className="centered-container">
+                        {
+                            isLoginError &&
+                            <div className="alert alert-danger" role="alert">
+                                {loginErrorMessage}
+                            </div>
+                        }
+                        {
+                            isLoading &&
+                            <div>
+                                <div className="spinner-border text-primary" role="status"/>
+                                <span className="text-primary">Loading...</span>
+                            </div>
+                        }
+                    </div>
+                </section>
+            </div>
+        )
 }
 
 export default Login;
