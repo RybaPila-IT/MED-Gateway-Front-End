4aae38949f39b08aa513bf46056c0b3c596fa823
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-04-13 10:31:30 +0200

    Add basic public home page

--- src/routes/public/Home.js
+++ src/routes/public/Home.js
@@ -1,9 +1,25 @@
+import {FaHammer} from 'react-icons/fa'
+
 function Home() {
     return (
-        <div>
-            <h1>
-                This is where my Home Page will be...
-            </h1>
+        <div className="centered-container">
+            <div style={{width: '30%'}}>
+                <h1>MED-Gateway system</h1>
+                <p>
+                    As you might have already guess this page is under construction.
+                    Feel free to move around and use the system. The CSS styling and
+                    visual design will be the last stage of the system implementation.
+                </p>
+                <FaHammer style={
+                    {
+                        fontSize: '20rem',
+                        paddingTop: '10%',
+                        paddingBottom: '10%'
+                    }}/>
+                <h4 style={{paddingTop: '10%'}}>
+                    Author: Radosław Radziukiewicz
+                </h4>
+            </div>
         </div>
 
     )
