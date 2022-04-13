4f57d0a1e0a69aa1cc2a5dd4dcb6be736eacc91a
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-04-13 11:40:44 +0200

    Add card CSS with hard-coded values

--- src/routes/protected/Dashboard.js
+++ src/routes/protected/Dashboard.js
@@ -1,7 +1,17 @@
+import fetus from '../../fetus.png';
+
 const Dashboard = () => {
     return (
         <div className="centered-container">
-            This is where the user dashboard will be...
+            <div className="product-card">
+                <img className="product-img" src={fetus} alt="Fetus" />
+                <div className="product-title">
+                    Fetal-Net
+                </div>
+                <div className="product-description">
+                    This is the Fetal-Net short description
+                </div>
+            </div>
         </div>
     )
 }
